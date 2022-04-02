7b1028c75bb5ec8ef4c4ac88bd51f81f34004ee8
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-03 00:54:49 +0530

    refactor2 added

--- src/main/java/Test/TestMaximum.java
+++ src/main/java/Test/TestMaximum.java
@@ -4,16 +4,6 @@ public class TestMaximum<T extends Comparable<T>> {
 	private T secondNumber;
 	private T thirdNumber;
 
-	private T floatNumber1;
-	private T floatNumber2;
-	private T floatNumber3;
-	
-	private T firstWord;
-	private T secondWord;
-	private T thirdWord;
-	
-	
-
 	public TestMaximum(T firstNumber, T secondNumber, T thirdNumber) {
 		this.firstNumber = firstNumber;
 		this.secondNumber = secondNumber;
@@ -44,56 +34,6 @@ public class TestMaximum<T extends Comparable<T>> {
 		this.thirdNumber = thirdNumber;
 	}
 
-	public T getFloatNumber1() {
-		return floatNumber1;
-	}
-
-	public void setFloatNumber1(T floatNumber1) {
-		this.floatNumber1 = floatNumber1;
-	}
-
-	public T getFloatNumber2() {
-		return floatNumber2;
-	}
-
-	public void setFloatNumber2(T floatNumber2) {
-		this.floatNumber2 = floatNumber2;
-	}
-
-	public T getFloatNumber3() {
-		return floatNumber3;
-	}
-
-	public void setFloatNumber3(T floatNumber3) {
-		this.floatNumber3 = floatNumber3;
-	}
-	
-	
-
-	public T getFirstWord() {
-		return firstWord;
-	}
-
-	public void setFirstWord(T firstWord) {
-		this.firstWord = firstWord;
-	}
-
-	public T getSecondWord() {
-		return secondWord;
-	}
-
-	public void setSecondWord(T secondWord) {
-		this.secondWord = secondWord;
-	}
-
-	public T getThirdWord() {
-		return thirdWord;
-	}
-
-	public void setThirdWord(T thirdWord) {
-		this.thirdWord = thirdWord;
-	}
-
 	
 	public void findMaximum(T firstNumber, T secondNumber, T thirdNumber) {
 		if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
@@ -104,7 +44,7 @@ public class TestMaximum<T extends Comparable<T>> {
 			System.out.println("Maximum: " + thirdNumber);
 	}
 
-public static void main(String[] args) {
+ public static void main(String[] args) {
 	System.out.println("welcome to maximum problem");
 	TestMaximum testmaximum = new TestMaximum(2, 1, 3);
 	testmaximum.findMaximum(2,1,3);
@@ -114,6 +54,5 @@ public static void main(String[] args) {
 	
 	TestMaximum testmaximum3 = new TestMaximum("Mango","Pineapple","Kiwi");
 	testmaximum3.findMaximum("Mango","Pineapple","Kiwi");
-
 }
 }
